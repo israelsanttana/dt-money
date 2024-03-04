d9eeaa2049e7148d78e659d658edf95323d1a84c
israelsanttana <israelsantanasantos7@gmail.com>
2024-03-04 00:05:56 -0300

    feat: adiciona projeto

---
+++ src/vite-env.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="vite/client" />
+// declarations.d.ts ou um arquivo específico para imagens
+declare module "*.png" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.jpg" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.jpeg" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.gif" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.svg" {
+    const value: any;
+    export default value;
+}
+
