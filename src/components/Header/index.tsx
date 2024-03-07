29e2d1d07f0048e31fb7dd642d6e2d3961409ded
israelsanttana <israelsantanasantos7@gmail.com>
2024-03-07 00:38:53 -0300

    feat: adiciona estilização do modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from '../../assets/logo.svg';
+import { NewTransactionModal } from '../NewTransactionModal';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export function Header() {
@@ -14,15 +15,7 @@ export function Header() {
                         <NewTransactionButton>Nova transação</NewTransactionButton>
                     </Dialog.Trigger>
 
-                    <Dialog.Portal>
-                        <Dialog.Overlay />
-                        <Dialog.Content>
-
-                            <Dialog.Close />
-                            <Dialog.Title>Nova transação</Dialog.Title>
-
-                        </Dialog.Content>
-                    </Dialog.Portal>
+                    <NewTransactionModal />
                 </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
